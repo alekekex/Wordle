f36b35d26b6196304fc254f48bc885cece2d5062
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-19 16:32:12 +0800

    added letter class

---
+++ Letter.java
@@ -0,0 +1,40 @@
+public class Letter {
+    private static final String GREEN = "\\u001B[32m";
+    private static final String YELLOW = "\\u001B[33m";
+    private static final String GRAY = "\\u001B[38;5;244m";
+    private static final String RESET = "\\u001B[0m";
+    private String letter;
+    private String color;
+
+    public Letter(String letter) {
+        this.letter = letter;
+        this.color = null;
+    }
+
+    public String getLetter() {
+        return this.letter;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColoredLetter() {
+        String coloredLetter = this.letter;
+
+        if(this.color != null) {
+            if(this.color.equals("GREEN"))
+                coloredLetter = GREEN + this.letter + RESET;
+            else if(this.color.equals("YELLOW"))
+                coloredLetter = YELLOW + this.letter + RESET;
+            else if(this.color.equals("GRAY"))
+                coloredLetter = GRAY + this.letter + RESET;
+        }
+
+        return coloredLetter;
+    }
+}
