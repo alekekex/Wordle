4d1e3042ea08f0da9913a3b46afd03ac1b05bd00
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-19 13:14:22 +0800

    added file reader method

--- Reader.java
+++ Reader.java
@@ -1,3 +1,21 @@
-public class Reader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Reader {
+    public List<String> readWords(String filePath) throws FileNotFoundException, IOException {
+        List<String> lines = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath));) {
+            String line;
+
+            while((line = br.readLine()) != null)
+                lines.add(line);
+        }
+
+        return lines;
+    }
 }
